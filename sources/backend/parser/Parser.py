e7997eea41f8b11ff290f231470153ea2c5a14e6
ricardaPoz <santaspolusa@gmail.com>
2022-12-04 01:24:05 +0300

    Completed file Parser.py

--- sources/backend/parser/Parser.py
+++ sources/backend/parser/Parser.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 import numpy as np
-from collections import namedtuple
 
 
 def data_transform():
@@ -10,27 +9,29 @@ def data_transform():
         lambda file_name: not file_name.__contains__('txt'), os.listdir(PATH_DIR_FILE_SOURCE)))
     REGIONS_NAMES: list[str] = get_regions_names(PATH_DIR_FILE_SOURCE)
 
-
-    df_result:pd.DataFrame = pd.DataFrame(np.array(range(0, 81)), columns=['Age'])
+    region_result = dict.fromkeys(REGIONS_NAMES, pd.DataFrame(
+        np.array(range(0, 81)), columns=['Age']))
 
     for file_name in SOURCE_FILE_NAMES:
         xlsx_file: pd.ExcelFile = pd.ExcelFile(
             f"{PATH_DIR_FILE_SOURCE}\{file_name}")
         name_pages: list[str | int] = xlsx_file.sheet_names
 
-
         for pages in name_pages:
             df: pd.DataFrame = pd.read_excel(xlsx_file, pages)
 
             first_i, second_i = get_index_by_name(file_name)
 
-            if df.values[first_i[0], first_i[1]] == "Белгородская область":
-                year_and_population: list = df.values[second_i[0]:, second_i[1]:second_i[2]].tolist()
+            if df.values[first_i[0], first_i[1]] in region_result.keys():
+                name_region = df.values[first_i[0], first_i[1]]
+
+                year_and_population: list = df.values[second_i[0]:, second_i[1]:second_i[2]].tolist(
+                )
 
                 year_and_population: list = year_and_population[:get_remove_index(
                     year_and_population) + 1]
                 year_and_population: list = list(filter(lambda v: not (
-                    v[0].__contains__('-') or v[0].__contains__('–')), year_and_population))
+                    str(v[0]).__contains__('-') or str(v[0]).__contains__('–')), year_and_population))
 
                 summa_last_two_element: int = sum(
                     map(lambda v: v[1], year_and_population[len(year_and_population) - 2:]))
@@ -38,17 +39,17 @@ def data_transform():
                     year_and_population) - 2]
                 year_and_population.append(['80', summa_last_two_element])
 
-                df_result[f'{file_name[:4]}'] = list(map(lambda v: v[1], year_and_population))
+                region_result.get(name_region)[f'{file_name[:4]}'] = list(
+                    map(lambda v: v[1], year_and_population))
         print(file_name)
-    
-    df_result.to_csv(f"data/regions/Белгород.csv", sep=';')
 
-     
+    for key, df in region_result.items():
+        df.to_csv(f"data/regions/{key}.csv", sep=';', index=False)
 
 
 def get_remove_index(array: list) -> int:
     for elem in array:
-        if elem[0].__contains__('85'):
+        if str(elem[0]).__contains__('85'):
             return array.index(elem)
 
 
@@ -67,8 +68,3 @@ def get_index_by_name(file_name: str):
 
 
 data_transform()
-# df_result:pd.DataFrame = pd.DataFrame(np.array(range(0, 81)), columns=['Age'])
-# print(df_result)
-# x = '2022.xlsx'
-# print(x[:4])
-
